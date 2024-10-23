96987469b883c7c938b30581a4df3682bea49d61
Jackson Merle <jackson.merle@gmail.com>
2024-10-23 11:34:14 +0900

    feat: implemented onClick action for each region on map

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Map from './Map.tsx'
 
 export default function App() {
 
-	const [region, selectRegion] = useState()
+	const [region, setRegion] = useState<string | null>()
 
-	function regionSelector () {
-		
+	useEffect(() => {
+		pathFetcher();
+	})
+
+	function pathFetcher () {
+		const paths = document.querySelectorAll('path');
+		for (let i = 0; i < paths.length; i++) {
+			paths[i].addEventListener('click', () => {console.log(paths[i].getAttribute('region-name'))})
+		}
 	}
 
 	return (
