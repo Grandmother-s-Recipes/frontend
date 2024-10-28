281096b04fc28d8733917a064a573acc61c3b922
Jason Sagalow <jrssagalow@gmail.com>
2024-10-28 16:51:15 +0900

    Feat: separate login and registration forms

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -2,13 +2,14 @@ import React, {useState} from 'react';
 import "../styles/loginForm.css";
 
 type loginProps = {
-    handleLoggedInState: Function,
+    handleLoggedInState: (loggedIn: boolean) => void,
 }
 
 const LoginForm: React.FC<loginProps> = ({ handleLoggedInState }) => {
 
     const [loginError, setLoginError] = useState<boolean>(false);
     const [loginFailed, setLoginFailed] = useState<boolean>(false);
+    const [loginOrRegister, setLoginOrRegister] = useState<string>('login');
     const URL: string = import.meta.env.VITE_API_URL;
 
     async function loginFunction() {
@@ -33,15 +34,21 @@ const LoginForm: React.FC<loginProps> = ({ handleLoggedInState }) => {
                 console.error('Login failed!');
                 setLoginFailed(true);
             }
-        } catch (error) {
+        } catch {
             setLoginError(true);
         }
     }
 
     async function registerFunction() {
-        const username = (document.getElementById("usernameEntry") as HTMLInputElement).value;
-        const password = (document.getElementById("passwordEntry") as HTMLInputElement).value;
-        
+        const username = (document.getElementById("usernameRegister") as HTMLInputElement).value;
+        const password = (document.getElementById("passwordRegister") as HTMLInputElement).value;
+        const confirmPassword = (document.getElementById("confirmPasswordRegister") as HTMLInputElement).value;
+
+        if (password !== confirmPassword) {
+            alert("Passwords don't match!");
+            return;
+        }
+
         try {
             const response = await fetch(`${URL}/register`, {
                 method: 'POST',
@@ -53,14 +60,12 @@ const LoginForm: React.FC<loginProps> = ({ handleLoggedInState }) => {
             });
 
             if (response.ok) {
-                const data = await response.json();
-                sessionStorage.setItem('user_id', data.user_id);
-                handleLoggedInState(true);
+                setLoginOrRegister('login');
             } else {
                 setLoginFailed(true);
                 console.error('Registration failed');
             }
-        } catch (error) {
+        } catch {
             setLoginError(true);
         }
     }
@@ -80,7 +85,7 @@ const LoginForm: React.FC<loginProps> = ({ handleLoggedInState }) => {
                         <p>Sorry about that</p>
                         <button onClick={returnToLogin}>Return to Login</button>
                     </>
-                ) : (
+                ) : loginOrRegister === 'login' ? (
                     <div className="loginForm">
                         <input id="usernameEntry" placeholder="Username" type="text"></input>
                         <br/>
@@ -89,7 +94,21 @@ const LoginForm: React.FC<loginProps> = ({ handleLoggedInState }) => {
                         <div className="buttonGroup">
                             <button onClick={loginFunction}>Login</button>
                             <br/>
+                            <button onClick={() => setLoginOrRegister('register')}>Register?</button>
+                        </div>
+                    </div>
+                ) : ( //loginOrRegister = 'register'
+                    <div className="loginForm">
+                        <input id="usernameRegister" placeholder="Username" type="text"></input>
+                        <br/>
+                        <input id="passwordRegister" placeholder="Password" type="password"></input>
+                        <br/>
+                        <input id="confirmPasswordRegister" placeholder="Confirm Password" type="password"></input>
+                        <br/>
+                        <div className="buttonGroup">
                             <button onClick={registerFunction}>Register</button>
+                            <br/>
+                            <button onClick={() => setLoginOrRegister('login')}>Back to login?</button>
                         </div>
                     </div>
                 )
